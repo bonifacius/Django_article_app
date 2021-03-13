0b14a9deb08750ac36605e2c3ef755351f82b375
Roman Basmanov <basmanovroman@mail.ru>
2021-03-13 23:39:18 +0500

    start project

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Django_article_app
+Инструкция к запуску:
+
+1. Клонируем репозиторий: git clone
+
+2. Переходим в папку с проектом который хотим запустить
+Создаем виртуальное окружение: python -m venv test-django
+
+3. Активируем окружение: source test-django/bin/activate
+
+4. Устанавливаем необходимые пакеты: pip install -r requirements.txt
+
+5. Делаем миграцию БД: python manage.py migrate
+
+6. Запускаем: python manage.py runserver
+
+
---
+++ boniface/news/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Articles(models.Model):
+    title = models.CharField('Название статьи', max_length=50)
+    anons = models.CharField('Анонс', max_length=255)
+    full_text = models.TextField('Статья')
+    date = models.DateTimeField('Дата публикации')
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f'/news/{self.pk}'
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
